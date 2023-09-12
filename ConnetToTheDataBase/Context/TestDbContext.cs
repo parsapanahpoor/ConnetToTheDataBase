9f9a1775a73e97ddc18c4cd73706c9aee48ff48f
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-09-12 15:16:34 +0330

    Add project files.

---
+++ ConnetToTheDataBase/Context/TestDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ConnetToTheDataBase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConnetToTheDataBase
+{
+    public class TestDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-JCN2C1I\\MSSQLSERVER2019;Initial Catalog=TestDbConnetion;Integrated Security=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
